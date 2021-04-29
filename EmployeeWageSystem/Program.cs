0160c13c636efc6c2228b2b8657d0d4108cb8d0c
bhu087 <bhu087@gmail.com>
2021-04-29 12:53:18 +0530

    [Bhushan] Refactor. User case 6 for monthly work hours 100 or 20 days

--- EmployeeWageSystem/Program.cs
+++ EmployeeWageSystem/Program.cs
@@ -11,30 +11,36 @@ namespace EmployeeWageSystem
             const int PART_TIME_HOUR = 4;
             const int WORKING_DAYS_PER_MONTH = 20;
             const int IS_ATTEND = 0;
-            int dailyWage = 0, attendence = 0, empHour = 0, monthlyWage = 0;
+            const int MONTHLY_HOUR = 100;
+            int dailyWage, attendence, empHour, monthlyWage = 0, workHours = 0;
             Console.WriteLine("Welcome to Employee Wage Computation Program");
             Random random = new Random();
             for (int i = 0; i < WORKING_DAYS_PER_MONTH; i++)
             {
-                attendence = random.Next(0, 3);
-                switch (attendence)
+                if (workHours < MONTHLY_HOUR)
                 {
-                    case IS_ATTEND:
-                        Console.WriteLine("Employee is present");
-                        empHour = FULL_DAY_HOUR;
-                        break;
-                    case 1:
-                        Console.WriteLine("Part time employee is Present");
-                        empHour = PART_TIME_HOUR;
-                        break;
-                    default:
-                        Console.WriteLine("Employee is absent");
-                        empHour = 0;
-                        break;
+                    attendence = random.Next(0, 3);
+                    switch (attendence)
+                    {
+                        case IS_ATTEND:
+                            Console.WriteLine("Employee is present");
+                            empHour = FULL_DAY_HOUR;
+                            break;
+                        case 1:
+                            Console.WriteLine("Part time employee is Present");
+                            empHour = PART_TIME_HOUR;
+                            break;
+                        default:
+                            Console.WriteLine("Employee is absent");
+                            empHour = 0;
+                            break;
+                    }
+                    dailyWage = WAGE_PER_HOUR * empHour;
+                    workHours += empHour;
+                    monthlyWage += dailyWage;
                 }
-                dailyWage = WAGE_PER_HOUR * empHour;
-                monthlyWage += dailyWage;
             }
+            Console.WriteLine(workHours);
             Console.WriteLine(monthlyWage);
         }
     }
