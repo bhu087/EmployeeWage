c58ebd826da4d83bdfb96b99016d37c8118fb31b
bhu087 <bhu087@gmail.com>
2021-04-29 12:50:27 +0530

    [Bhushan] Refactor. User case 5 Calculating monthly wage

--- EmployeeWageSystem/Program.cs
+++ EmployeeWageSystem/Program.cs
@@ -9,26 +9,33 @@ namespace EmployeeWageSystem
             const int WAGE_PER_HOUR = 20;
             const int FULL_DAY_HOUR = 8;
             const int PART_TIME_HOUR = 4;
+            const int WORKING_DAYS_PER_MONTH = 20;
             const int IS_ATTEND = 0;
-            int dailyWage = 0, attendence = 0, empHour = 0;
+            int dailyWage = 0, attendence = 0, empHour = 0, monthlyWage = 0;
             Console.WriteLine("Welcome to Employee Wage Computation Program");
             Random random = new Random();
-            attendence = random.Next(0,1);
-            switch (attendence)
+            for (int i = 0; i < WORKING_DAYS_PER_MONTH; i++)
             {
-                case IS_ATTEND:
-                    Console.WriteLine("Employee is present");
-                    empHour = FULL_DAY_HOUR;
-                    break;
-                case 1:
-                    Console.WriteLine("Part time employee is Present");
-                    empHour = PART_TIME_HOUR;
-                    break;
-                default:
-                    empHour = 0;
-                    break;
+                attendence = random.Next(0, 3);
+                switch (attendence)
+                {
+                    case IS_ATTEND:
+                        Console.WriteLine("Employee is present");
+                        empHour = FULL_DAY_HOUR;
+                        break;
+                    case 1:
+                        Console.WriteLine("Part time employee is Present");
+                        empHour = PART_TIME_HOUR;
+                        break;
+                    default:
+                        Console.WriteLine("Employee is absent");
+                        empHour = 0;
+                        break;
+                }
+                dailyWage = WAGE_PER_HOUR * empHour;
+                monthlyWage += dailyWage;
             }
-            dailyWage = WAGE_PER_HOUR * empHour;
+            Console.WriteLine(monthlyWage);
         }
     }
 }
