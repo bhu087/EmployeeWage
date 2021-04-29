3bdda876ea0d32565ad3dc0bada41115949bc9b0
bhu087 <bhu087@gmail.com>
2021-04-29 11:28:04 +0530

    [Bhushan] Refactor. User case 2 for calculate daily wage

--- EmployeeWageSystem/Program.cs
+++ EmployeeWageSystem/Program.cs
@@ -6,10 +6,14 @@ namespace EmployeeWageSystem
     {
         static void Main(string[] args)
         {
+            const int WAGE_PER_HOUR = 20;
+            const int FULL_DAY_HOUR = 8;
+            int dailyWage = 0;
             int attendence = 0;
             Console.WriteLine("Welcome to Employee Wage Computation Program");
             Random random = new Random();
             attendence = random.Next(0,1);
+            dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
         }
     }
 }
