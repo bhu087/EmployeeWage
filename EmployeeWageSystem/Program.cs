e1087799a5c66960ff38eb5f23e810da1ea1f447
bhu087 <bhu087@gmail.com>
2021-04-29 11:17:58 +0530

    [Bhushan] Refactor. User case 1 is added for generation of random number for attendence

--- EmployeeWageSystem/Program.cs
+++ EmployeeWageSystem/Program.cs
@@ -6,7 +6,10 @@ namespace EmployeeWageSystem
     {
         static void Main(string[] args)
         {
+            int attendence = 0;
             Console.WriteLine("Welcome to Employee Wage Computation Program");
+            Random random = new Random();
+            attendence = random.Next(0,1);
         }
     }
 }
